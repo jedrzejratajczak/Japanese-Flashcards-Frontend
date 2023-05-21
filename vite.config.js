53e7bf6fb57c5095921c7aae4fa349c9ffd2abeb
jedrzejratajczak <jedrzej.ratajczak@htdevelopers.com>
2023-05-21 22:35:14 +0200

    build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: 'https://jedrzejratajczak.github.io/Japanese-Flashcards-Frontend/',
   server: {
     port: 3000,
     strictPort: true
